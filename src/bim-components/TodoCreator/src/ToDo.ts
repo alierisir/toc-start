c72b18e85944720e8aa5fe3431f8be3ca19bae23
ALE <alierisir@gmail.com>
2024-06-28 00:15:46 +0300

    one todo class

--- src/bim-components/TodoCreator/src/ToDo.ts
+++ src/bim-components/TodoCreator/src/ToDo.ts
@@ -3,7 +3,16 @@ import { generateUUID } from "three/src/math/MathUtils.js";
 import * as THREE from "three";
 import { TodoCard } from "./TodoCard";
 import { TodoCreator } from "..";
-import { IToDo, ToDoPriority, ToDoStatus } from "../../../classes/ToDo";
+import { monthsAfterToday } from "../../../classes/CustomFunctions";
+
+export type ToDoStatus = "active" | "completed" | "overdue";
+export type ToDoPriority = "low" | "normal" | "high";
+export interface IToDo {
+  task: string;
+  status: ToDoStatus;
+  deadline: Date;
+  priority: ToDoPriority;
+}
 
 export class ToDo extends OBC.Component<ToDo> implements OBC.Disposable {
   enabled: boolean = true;
@@ -30,7 +39,72 @@ export class ToDo extends OBC.Component<ToDo> implements OBC.Disposable {
     this.status = data.status;
     this.taskId = id;
     this.setup();
+    this.checkStatus();
     this.createCard();
+  }
+
+  private checkStatus() {
+    const today = new Date();
+    if (this.deadline >= today && this.status === "active") {
+      //console.log("Task is waiting to be done.");
+      this.setStatus("active");
+      return this.status;
+    }
+    if (this.deadline >= today && this.status === "completed") {
+      //console.log("Task is completed early.");
+      this.setStatus("completed");
+      return this.status;
+    }
+    if (this.deadline >= today && this.status === "overdue") {
+      //console.log("There is still time for the task to be completed, changing status to 'active'.");
+      this.setStatus("active");
+      return this.status;
+    }
+    if (this.deadline < today && this.status === "active") {
+      //console.log("Task is overdue.");
+      this.setStatus("overdue");
+      return this.status;
+    }
+    if (this.deadline < today && this.status === "completed") {
+      //console.log("Task is already completed.");
+      this.setStatus("completed");
+      return this.status;
+    }
+    if (this.deadline < today && this.status === "overdue") {
+      //console.log("Task couldn't be completed in time!");
+      this.setStatus("overdue");
+      return this.status;
+    }
+  }
+
+  setDeadline(date: Date) {
+    this.deadline = date;
+    this.checkStatus();
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  setStatus(status: ToDoStatus) {
+    this.status = status;
+  }
+
+  toggleStatus(status: ToDoStatus) {
+    if (status === "active") return this.setStatus("completed");
+    if (status === "completed") {
+      if (this.deadline < new Date()) {
+        return this.setStatus("overdue");
+      } else {
+        return this.setStatus("active");
+      }
+    }
+    if (status === "overdue") {
+      console.log(
+        "note to self: add a date input or delete task options here. for now deadline is updated to 1 month from today!"
+      );
+      return this.setDeadline(monthsAfterToday(1));
+    }
   }
 
   private async setup() {
